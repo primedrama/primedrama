1f7ecc50e3b82ad6ca6c535f1be1f4cc030546bd
primedrama <rosietorba@gmail.com>
2026-01-06 09:07:10 +0800

    home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,5 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'e34316a6fe456fe22b50c4033980b236
+';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
@@ -119,5 +120,6 @@ const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
       displayList(tvShows, 'tvshows-list');
       displayList(anime, 'anime-list');
     }
+
 
     init();
